4e3eba5dffbfc68ac894faa2a077eef811a2c346
ktoprakucar <toprakucar@gmail.com>
2017-03-29 17:26:47 +0300

    add: fillOutOfMatrix

--- src/main/java/operations/Editings.java
+++ src/main/java/operations/Editings.java
@@ -5,12 +5,22 @@ package operations;
  */
 public class Editings {
 
-  public int[][] fillOutOfMatrix(int filterSize, int[][] matrix){
+  public static int[][] fillOutOfMatrix(int filterSize, int[][] matrix) {
     int expansionSize = calculateAdditionalPixelsForEachDimension(filterSize);
-    return null;
+    int[][] filledMatrix = new int[matrix.length + expansionSize * 2][matrix[0].length + expansionSize * 2];
+    for (int i = 0; i < filledMatrix.length; i++) {
+      for (int j = 0; j < filledMatrix[0].length; j++) {
+        if (i < expansionSize || j < expansionSize || i > filledMatrix.length - expansionSize -1 || j > filledMatrix[0].length - expansionSize - 1) {
+          filledMatrix[j][i] = 0;
+        } else {
+          filledMatrix[i][j] = matrix[i - expansionSize][j - expansionSize];
+        }
+      }
+    }
+    return filledMatrix;
   }
 
   public static int calculateAdditionalPixelsForEachDimension(int filterSize) {
-    return (int) Math.ceil(filterSize/2);
+    return (int) Math.ceil(filterSize / 2);
   }
 }
--- src/test/java/EditingsTests.java
+++ src/test/java/EditingsTests.java
@@ -9,14 +9,35 @@ import static junit.framework.TestCase.assertEquals;
  */
 public class EditingsTests {
 
+
   @Before
-  public void setUp(){
+  public void setUp() {
 
   }
 
   @Test
-  public void test_calculate_additional_pixels(){
+  public void test_calculate_additional_pixels() {
     int expansion = Editings.calculateAdditionalPixelsForEachDimension(7);
     assertEquals(3, expansion);
+  }
+
+  @Test
+  public void test_expand_matrix() {
+    int[][] matrix = new int[5][5];
+    int[][] filledMatrix = Editings.fillOutOfMatrix(7, matrix);
+    assertEquals(11, filledMatrix.length);
+    assertEquals(11, filledMatrix[0].length);
+  }
+
+  @Test
+  public void test_fill_out_of_matrix(){
+    int[][] matrix = new int [][]{
+            new int [] {1,2,3,4,5},
+            new int [] {1,2,3,4,5},
+            new int [] {1,2,3,4,5},
+            new int [] {1,2,3,4,5},
+            new int [] {1,2,3,4,5}
+    };
+    int[][] filledMatrix = Editings.fillOutOfMatrix(7, matrix);
   }
 }
